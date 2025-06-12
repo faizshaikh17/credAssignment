a972fa8edbcbbcf23f55ceb119bccc3cea7a82a0
Faiz Shaikh <mohdfaiz8101@gmail.com>
2025-06-13 02:23:03 +0530

    fix: minor

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import CreditCardsList from './ui/components/CreditCardsList';
 
 export default function Page() {
   return (
-    <div className="min-h-screen relative flex flex-col items-center justify-start bg-black">
+    <div className="min-h-screen md:p-0 p-2 relative flex flex-col items-center justify-start bg-black">
       <Suspense fallback={<div className="text-white">Loading...</div>}>
         <CreditCardsList />
       </Suspense>
